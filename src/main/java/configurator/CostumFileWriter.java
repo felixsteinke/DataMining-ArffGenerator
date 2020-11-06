013f0ac554eb5fa18f1d00f0ff0b30a2a392a31a
Florian Grabowski <82grfl1bif@hft-stuttgart.de>
2020-11-06 13:25:46 +0100

    Costum Files Vorbereitung

---
+++ src/main/java/configurator/CostumFileWriter.java
@@ -0,0 +1,16 @@
+package configurator;
+
+public class CostumFileWriter {
+
+    String a = "@attribute";
+    String r = "@relation";
+    String n = "numeric";
+    String d = "@data";
+    String file = "src/main/resources/GruppeF.arff";
+
+    StringBuilder stringBuilder = new StringBuilder();
+
+    public static void main(String[] args) {
+
+    }
+}
