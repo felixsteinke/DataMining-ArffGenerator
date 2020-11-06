cac8ad0161d56d4e28f0c815d4fb32e9378e835e
Florian Grabowski <82grfl1bif@hft-tuttgart.de>
2020-11-06 23:59:07 +0100

    Attribute in costum file writer

--- src/main/java/configurator/CostumFileWriter.java
+++ src/main/java/configurator/CostumFileWriter.java
@@ -1,16 +1,51 @@
 package configurator;
 
+import dataProviders.DataProvider;
+
+import java.io.*;
+import java.util.ArrayList;
+
 public class CostumFileWriter {
 
-    String a = "@attribute";
-    String r = "@relation";
-    String n = "numeric";
-    String d = "@data";
-    String file = "src/main/resources/GruppeF.arff";
+    private static StringBuilder stringBuilder = new StringBuilder();
+    private static DataProvider dataProvider = new DataProvider();
 
-    StringBuilder stringBuilder = new StringBuilder();
+    private static String a = "@attribute ";
+    private static String iT = "_in_Text";
+    private static String iS = "_in_Subject";
+    private static String r = "@relation spamGruppeF";
+    private static String n = "numeric";
+    private static String d = "@data";
+    private static String file = "src/main/resources/GruppeF.arff";
+    private static String nl = "\n";
 
     public static void main(String[] args) {
 
+        stringBuilder.append(r);
+        stringBuilder.append(nl);
+        stringBuilder.append(nl);
+
+        appendAttributeFromListAs(dataProvider.getBlackListedWords(),iT);
+        appendAttributeFromListAs(dataProvider.getBlackListedWords(),iS);
+        appendAttributeFromListAs(dataProvider.getWhiteListedWords(),iT);
+        appendAttributeFromListAs(dataProvider.getWhiteListedWords(),iS);
+
+        String result = stringBuilder.toString();
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))){
+            bufferedWriter.write(result);
+            bufferedWriter.flush();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void appendAttributeFromListAs(ArrayList<String> wordList,String context){
+        for (String word: wordList) {
+            stringBuilder.append(a);
+            //TODO remove whitespace from word
+            stringBuilder.append(word);
+            stringBuilder.append(context);
+            stringBuilder.append(nl);
+        }
     }
 }
